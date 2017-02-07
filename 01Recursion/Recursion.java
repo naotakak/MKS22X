de9db88a63f95281f14db62a982a0ae031bb0ad8
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-02-06 22:37:40 -0500

    finished Recursion

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,22 @@
+public class Recursion {
+
+    public static double sqrt(double n) {
+	if (n < 0) {
+	    throw new IllegalArgumentException("n must be greater than 0");
+	}
+	return helper(n, n / 2);
+    }
+
+    private static double helper(double n, double guess) {
+	if ((n - guess * guess) / n <= .000000000000001 && n - guess * guess >= 0) {
+	    return guess;
+	}
+	System.out.println(guess);
+	return helper(n, (n / guess + guess) / 2);
+    }
+    
+    public static String name() {
+	return "Kinoshita,Naotaka";
+    }
+
+}
