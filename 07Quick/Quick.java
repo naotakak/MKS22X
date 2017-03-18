574f3e8b8903d1b38d1adf9c4ceac0894df98add
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-03-18 15:14:40 -0400

    fixed part

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,16 +1,8 @@
 import java.util.*;
 public class Quick {
 
-    public static int[] quicksort(int[]data) {
-	int[]fin = new int[data.length];
-	for (int i = 0; i < data.length; i ++) {
-	    fin[i] = quickselect(data, i);
-	}
-	return fin;
-    }
-
     public static int quickselect(int[]data, int k) {
-	int start = 0;
+        int start = 0;
 	int end = data.length - 1;
 	for (int i = 0 ; i < data.length; i ++) {
 	    int partReturn = part(data, start, end);
@@ -27,28 +19,25 @@ public class Quick {
     private static int part(int[]data, int start, int end) {
 	int pivot = pickRandom(start, end);
 	int pivotThing = data[pivot];
-	for (int i = start; i <= end; i ++) {
-	    int data1 = data[i];
-	    if (data1 >= pivotThing && i < pivot) {
-		data = pushBack(data, i, pivot);
-		pivot = i;
+	int a = start;
+	int b = end - 1;
+	data[pivot] = data[end];
+	data[end] = pivotThing;
+        while (a <= b) {
+	    if (data[a] < data[b]) {
+		a ++;
 	    }
-	    if (data1 < pivotThing && i > pivot) {
-		data = pushBack(data, pivot, i);
-		pivot = i - 1;
+	    if (data[a] >= data[b]) {
+		int dataA = data[a];
+		data[a] = data[b];
+		data[b] = dataA;
+		b --;
 	    }
 	}
-	return pivot;
-    }
-
-    private static int[] pushBack(int[]data, int start, int end) {
-	int endThing = data[end];
-	for (int i = end; i > start; i --) {
-	    int thingBefore = data[i - 1];
-	    data[i] = thingBefore;
-	}
-	data[start] = endThing;
-	return data;
+	int finalSwap = data[a];
+	data[a] = data[end];
+	data[end] = finalSwap;
+	return a;
     }
     
     private static int pickRandom(int start, int end) {
@@ -58,14 +47,12 @@ public class Quick {
 
     public static void main(String[]args) {
 	int[]pivotary = new int[] {2, 10, 15, 23, 0,  5};
-	/*
 	System.out.println(quickselect(pivotary, 0));
 	System.out.println(quickselect(pivotary, 1));
 	System.out.println(quickselect(pivotary, 2));
 	System.out.println(quickselect(pivotary, 3));
 	System.out.println(quickselect(pivotary, 4));
 	System.out.println(quickselect(pivotary, 5));
-	*/
-	System.out.println(Arrays.toString(quicksort(pivotary)));
+	
     }
 }
