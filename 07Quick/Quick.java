5e71266b906ad6bacfd8289afc6a40bea203ad54
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-03-21 12:30:21 -0400

    part broken

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -19,29 +19,36 @@ public class Quick {
     private static int part(int[]data, int start, int end) {
 	int pivot = pickRandom(start, end);
 	int pivotThing = data[pivot];
-	int a = start;
-	int b = end - 1;
-	data[pivot] = data[end];
-	data[end] = pivotThing;
-        while (a <= b) {
-	    if (data[a] < data[b]) {
-		a ++;
+	int i = start;
+	int gt = end;
+	int lt = start;
+	data[pivot] = data[start];
+	data[start] = pivotThing;
+        while (i <= gt) {
+	    if (data[i] == data[lt]) {
+		i ++;
 	    }
-	    if (data[a] >= data[b]) {
-		int dataA = data[a];
-		data[a] = data[b];
-		data[b] = dataA;
-		b --;
+	    else if (data[i] < data[lt]) {
+		swap(data, i, lt);
+		lt ++;
+		i++;
+	    }
+	    else {
+		swap(data, i , gt);
+		gt --;
 	    }
 	}
-	int finalSwap = data[a];
-	data[a] = data[end];
-	data[end] = finalSwap;
-	return a;
+	return i;
     }
     
+    private static void swap(int[]data, int in1, int in2) {
+	int old = data[in1];
+	data[in1] = data[in2];
+	data[in2] = old;
+    }
+
     private static int pickRandom(int start, int end) {
-	int range = (end - start) + 1;
+	int range = (end - 1 - start) + 1;
 	return (int)(Math.random() * range) + start;
     }
 
