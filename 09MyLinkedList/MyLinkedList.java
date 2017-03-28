31661c3c2dd4357c9a7fd98e39aeba8432246b71
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-03-28 18:56:43 -0400

    finished

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -9,12 +9,46 @@ public class MyLinkedList {
     }
 
     public boolean add(int value) {
+	LNode n = new LNode(value);
+	LNode thing = start;
+	for (int i = 0; i < size - 1; i ++) {
+	    thing = thing.next;
+	}
+	thing.next = n;
 	size += 1;
-	LNode n = new LNode(value, start);
-	start = n;
 	return true;
     }
 
+    public void add(int index, int value) {
+	if (index >= size || index < 0) {
+	    throw new IndexOutOfBoundsException();
+	}
+	LNode addition = new LNode(value);
+	LNode thing = start;
+	for (int i = 0; i < index - 1; i ++) {
+	    thing = thing.next;
+	}
+	LNode oldNext = thing.next;
+	thing.next = addition;
+	addition.next = oldNext;
+	size += 1;
+    }
+
+    public int remove(int index) {
+	if (index >= size || index < 0) {
+	    throw new IndexOutOfBoundsException();
+	}
+	LNode thing = start;
+	for (int i = 0; i < index - 1; i ++) {
+	    thing = thing.next;
+	}
+	int val = (thing.next).value;
+	LNode newNext = thing.next.next;
+	thing.next = newNext;
+	size -= 1;
+	return val;
+    }
+	
     public int size() {
 	return size;
     }
@@ -32,32 +66,41 @@ public class MyLinkedList {
     }
 
     public int get(int index) {
-	if (index >= size) {
+        if (index >= size || index < 0) {
 	    throw new IndexOutOfBoundsException();
 	}
-	LNode old = start;
-	LNode thing = new LNode(0);
+	LNode thing = start;
 	for (int i = 0; i < index; i ++) {
-	    thing = start.next;
-	    start.next = thing;
+	    thing = thing.next;
 	}
 	return thing.value;
     }
 
     public int set(int index, int v) {
-	if (index >= size) {
+	if (index >= size || index < 0) {
 	    throw new IndexOutOfBoundsException();
 	}
 	int old;
-	LNode thing = new LNode(0);
+	LNode thing = start;
 	for (int i = 0; i < index; i ++) {
-	    thing = start.next;
+	    thing = thing.next;
 	}
 	old = thing.value;
 	thing.value = v;
 	return old;
     }
 
+    public int indexOf(int value) {
+	LNode thing = start;
+	for (int i = 0; i < size; i ++) {
+	    if (thing.value == value) {
+		return i;
+	    }
+	    thing = thing.next;
+	}
+	return -1;
+    }
+	
     public class LNode {
     
 	private int value;
@@ -74,12 +117,15 @@ public class MyLinkedList {
     }
 
     public static void main(String[]args) {
-	MyLinkedList e = new MyLinkedList(10);
-	for (int i = 9; i >= 0; i --) {
+	MyLinkedList e = new MyLinkedList(0);
+	for (int i = 1; i < 10; i ++) {
 	    e.add(i);
 	}
 	System.out.println(e);
-	System.out.println(e.get(9));
+        e.add(2,0);
+	System.out.println(e);
+	System.out.println(e.remove(2));
+	System.out.println(e);
     }
 }
 
