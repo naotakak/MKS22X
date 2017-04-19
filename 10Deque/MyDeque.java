a3888b3f3d75d667c779056c9b086cc81c0cc254
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-04-19 12:29:13 -0400

    started Deque

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,37 @@
+import java.util.*
+
+public class MyDeque extends Queue<String>{
+    
+    private Queue thing;
+    private int front;
+    private int back;
+
+    public MyDeque() {
+    }
+
+    public boolean add(String e) {
+	
+    }
+
+    public String element() {
+
+    }
+
+    public boolean offer(String e) {
+	
+    }
+
+    public String peek() {
+
+    }
+
+    public String poll() {
+
+    }
+
+    public String remove() {
+
+    }
+
+}
+    
