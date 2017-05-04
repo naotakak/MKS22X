026d4d8a9e4acca9a56c7bd2643836e9f53f64d4
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-05-03 21:56:07 -0400

    pushUp and pushDown not working yet

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -4,6 +4,7 @@ public class MyHeap {
     
     private ArrayList<String> heap;
     private int size;
+    private boolean max;
     
     public MyHeap() {
 	heap = new ArrayList<String>();
@@ -11,31 +12,36 @@ public class MyHeap {
 	size = 1;
     }
 
-    public String pop() {
+    public MyHeap(boolean m) {
+	max = m;
+	heap = new ArrayList<String>();
+	heap.add("");
+	size = 1;
+    }
+
+    public String remove() {
 	String ret = heap.get(1);
 	heap.set(1, heap.remove(size));
 	size -= 1;
-	pushBack(1);
+	pushDown();
 	return ret;
     }
 
-    public void add() {
-	
-    }
-
-    public void remove() {
-
+    public void add(String s) {
+	heap.add(s);
+	size += 1;
+	pushUp();
     }
 
     public String peek() {
 	return heap.get(1);
     }
 
-    private void pushForward(int index) {
+    private void pushUp() {
 
     }
 
-    private void pushBack(int index) {
+    private void pushDown() {
 
     }
 
