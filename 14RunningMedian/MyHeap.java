099ba4e01c3dfadf795f17ae1ae4d1ca211b05c0
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-05-08 12:29:15 -0400

    MyHeap problem

---
+++ 14RunningMedian/MyHeap.java
@@ -0,0 +1,96 @@
+import java.util.*;
+
+public class MyHeap {
+    
+    private ArrayList<Integer> heap;
+    private int size;
+    private boolean max = true;
+    
+    public MyHeap() {
+	heap = new ArrayList<Integer>();
+	heap.add(0);
+	size = 0;
+    }
+
+    public MyHeap(boolean m) {
+	max = m;
+	heap = new ArrayList<Integer>();
+	heap.add(0);
+	size = 0;
+    }
+
+    public Integer remove() {
+	Integer ret = heap.get(1);
+	heap.set(1, heap.remove(size));
+	size -= 1;
+	pushDown(1);
+	return ret;
+    }
+
+    public void add(Integer s) {
+	heap.add(s);
+	size += 1;
+	pushUp(size);
+    }
+
+    public Integer peek() {
+	return heap.get(1);
+    }
+
+    private void pushUp(int index) {
+	Integer temp;
+	if (max) {
+	    if (index > 1 && 
+		heap.get(index).compareTo(heap.get(index / 2)) > 0) {
+		System.out.println(index);
+		temp = heap.get(index / 2);
+		heap.set(index / 2, heap.get(index));
+		heap.set(index, temp);
+		pushUp(index / 2);
+	    }
+	}
+	else {
+	   if (index > 1 && 
+		heap.get(index).compareTo(heap.get(index / 2)) < 0) {
+		temp = heap.get(index / 2);
+		heap.set(index / 2, heap.get(index));
+		heap.set(index, temp);
+		pushUp(index / 2);
+	    }
+	}
+    }
+
+    public int getSize() {
+	return size;
+    }
+
+    private void pushDown(int index) {
+        Integer temp;
+	if (max) {
+	    if (index < size && 
+		heap.get(index).compareTo(heap.get(index * 2)) < 0) {
+		temp = heap.get(index * 2);
+		heap.set(index * 2, heap.get(index));
+		heap.set(index, temp);
+		pushDown(index * 2);
+	    }
+	}
+	else {
+	   if (index < size && 
+		heap.get(index).compareTo(heap.get(index * 2)) > 0) {
+		temp = heap.get(index * 2);
+		heap.set(index * 2, heap.get(index));
+		heap.set(index, temp);
+		pushDown(index * 2);
+	    }
+	}
+    }
+
+    public String toString() {
+	return (Arrays.toString(heap.toArray()));
+    }
+
+    public static void main (String[]args) {
+	MyHeap z = new MyHeap(true);
+    }
+}
---
+++ 14RunningMedian/RunningMedian.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class RunningMedian {
+
+    private MyHeap smaller;
+    private MyHeap larger;
+    private double median;
+
+    public RunningMedian() {
+	smaller = new MyHeap(true);
+	larger = new MyHeap(false);
+    }
+
+    public void add(Integer a) {
+	if (a.intValue() > median) {
+	    larger.add(a);
+	    System.out.println("A");
+	    if (larger.getSize() > smaller.getSize() + 1) {
+		smaller.add(larger.remove());
+	    }
+	}
+	else {
+	    smaller.add(a);
+	    System.out.println("B");
+	    if (smaller.getSize() > larger.getSize() + 1) {
+		larger.add(smaller.remove());
+
+	    }
+	}
+	median = updateMedian();
+    }
+
+    private double updateMedian() {
+	if (smaller.getSize() > larger.getSize()) {
+	    return smaller.peek().intValue();
+	}
+	else if (smaller.getSize() < larger.getSize()) {
+	    return larger.peek().intValue();
+	}
+	else {
+	    return (smaller.peek().intValue() + larger.peek().intValue()) / 2.0;
+	}
+    }
+
+    public double getMedian() {
+	return median;
+    }
+
+    public static void main (String[]args) {
+	RunningMedian a = new RunningMedian();
+	for (int i = 0; i < 10; i ++) {
+	    a.add(new Integer(i));
+	    System.out.println("" + i + " " + a.getMedian());
+	}
+    }
+}
